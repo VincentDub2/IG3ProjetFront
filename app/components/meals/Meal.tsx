2c096cd0afdbbb05e13f847af8a9927f886170e8
Vincent <vincent.dubuc@etu.umontpellier.fr>
2023-05-22 09:35:14 +0200

    Changement couleur icon

--- app/components/meals/Meal.tsx
+++ app/components/meals/Meal.tsx
@@ -14,7 +14,7 @@ const Meal: React.FC<MealProps> = ({ label, icon: Icon, foods }) => {
     return (
         <div className="relative flex bg-white shadow-sm rounded-lg p-4 mx-auto mb-4 flex-col min-w-full ">
             <div className="flex flex-row items-center space-x-2 text-gray-700 text-sm font-semibold">
-                <Icon size={18} className="text-neutral-600" />
+                <Icon size={18} className="text-neutral-600 text-rose-500" />
                 <div className="text-2xl">{label}</div>
             </div>
             <hr className="border-gray-200 my-2"/>
