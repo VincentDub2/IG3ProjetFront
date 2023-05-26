6e24b9eb7fdb688753ff6cd31909604d00e5dd9d
Vincent <vincent.dubuc@etu.umontpellier.fr>
2023-05-26 09:04:43 +0200

    Ajoute documentation

--- app/components/inputs/BrandSelect.tsx
+++ app/components/inputs/BrandSelect.tsx
@@ -1,21 +1,38 @@
+'use client';
 import React, { useState } from 'react';
 import Select from 'react-select';
 import useBrands from '@/app/hooks/useBrands';
-import {BrandSelectValue} from '@/app/types';
-import {ValueType} from "tailwindcss/types/config";
+import { BrandSelectValue } from '@/app/types';
 import { CSSObject } from '@emotion/react';
 import { OptionProps } from 'react-select';
 
+/**
+ * BrandSelect permet de sélectionner une marque.
+ * c'est un menu déroulant qui affiche toutes les marques disponibles.
+ */
 
+
+/**
+ * Propriétés du composant BrandSelect.
+ */
 interface BrandSelectProps {
     value?: BrandSelectValue;
     onChange: (value: BrandSelectValue | null) => void;
 }
 
+/**
+ * Composant BrandSelect permettant de sélectionner une marque.
+ */
 const BrandSelect: React.FC<BrandSelectProps> = ({ value, onChange }) => {
+    // Récupérer toutes les marques via le hook useBrands
     const { getAll } = useBrands();
     const brands = getAll();
 
+    /**
+     * Fonction de formatage de l'étiquette d'une option de sélection.
+     * @param option - L'option à formater.
+     * @returns Le contenu JSX de l'étiquette formatée.
+     */
     const formatOptionLabel = (option: BrandSelectValue) => (
         <div className="flex flex-row items-center gap-3 z-10 opacity-80">
             <div>{option.name}</div>
@@ -23,23 +40,24 @@ const BrandSelect: React.FC<BrandSelectProps> = ({ value, onChange }) => {
     );
 
     // Trouver l'option correspondante dans le tableau de marques
-    const selectedBrand = brands.find(brand => brand.id === value?.id);
+    const selectedBrand = brands.find((brand) => brand.id === value?.id);
 
-
+    /**
+     * Styles personnalisés pour les options de sélection.
+     */
     const customStyles = {
-        option: (provided :CSSObject , state : OptionProps<BrandSelectValue, boolean>) => ({
+        option: (provided: CSSObject, state: OptionProps<BrandSelectValue, boolean>) => ({
             ...provided,
             color: state.isSelected ? 'pink' : 'black',
             backgroundColor: state.isSelected ? 'white' : 'white',
-            backdropOpacity : 1,
+            backdropOpacity: 1,
         }),
         menu: (provided: CSSObject) => ({
             ...provided,
-            zIndex: 20
+            zIndex: 20,
         }),
         // autres styles personnalisés ici...
-    }
-
+    };
 
     return (
         <Select
@@ -55,13 +73,12 @@ const BrandSelect: React.FC<BrandSelectProps> = ({ value, onChange }) => {
                     onChange(selectedValue as BrandSelectValue);
                 }
             }}
-
             formatOptionLabel={formatOptionLabel}
             classNamePrefix="react-select"
             classNames={{
-                option:() => 'text-lg',
-                control:() => 'p-3 border-2',
-                input : () => 'text-lg',
+                option: () => 'text-lg',
+                control: () => 'p-3 border-2',
+                input: () => 'text-lg',
             }}
             theme={(theme) => ({
                 ...theme,
